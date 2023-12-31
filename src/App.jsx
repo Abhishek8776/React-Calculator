7a9a4db5e4f7a9898c9ef721ea96ddfec4a8549d
Abhishek P <abhishekpclt1999@gmail.com>
2023-12-31 19:19:41 +0530

    display numbers

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ButtonContainer from './components/ButtonsContainer'
+import { useState } from 'react'
+
+
+function App() {
+  let [displayText, setDisplay] = useState("")
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C"){
+      setDisplay("")
+    }else if (buttonText === "=") {
+      console.log(buttonText)
+    } else {
+      const newDisplayText = displayText + buttonText
+      setDisplay(newDisplayText)
+    }
+  }
+  return (
+    <div className='w-25 px-5 py-4 border border-2'>
+      <input type="text" className='w-100 form-control' value={displayText} readOnly />
+      <ButtonContainer onButtonClick={onButtonClick} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ButtonsContainer.jsx
@@ -0,0 +1,16 @@
+function ButtonContainer({ onButtonClick }) {
+  const ButtonNames = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', '.', '=', '/']
+
+
+  return ( 
+    <div className="row row-cols-4">
+      {ButtonNames.map((item) => 
+        <div className="col py-2" key={item}>
+          <button className='btn btn-secondary btn-lg' onClick={() => onButtonClick(item)}>{item}</button>
+        </div>
+      )}
+    </div>
+   );
+}
+
+export default ButtonContainer;
